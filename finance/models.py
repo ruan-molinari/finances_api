5e85a4536ada9f3adc6136bc85cf92ff9a62e8f8
zuanz <ruan.gri@proton.me>
2022-08-24 22:48:53 -0300

    Fix expense test

--- finance/models.py
+++ finance/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
-class Category(models.IntegerChoices):
-    OTHER = 0, 'Other'
-    FOOD = 1, 'Food'
-    HEALTH = 2, 'Health'
-    DWELLING = 3, 'Dwelling'
-    TRANSPORTATION = 4, 'Transportation'
-    EDUCATION = 5, 'Education'
-    ENTERTAINMENT = 6, 'Enterprise'
-    UNEXPECTED = 7, 'Unexpected'
+class Category(models.Choices):
+    OTHER = 'Other'
+    FOOD = 'Food'
+    HEALTH = 'Health'
+    DWELLING = 'Dwelling'
+    TRANSPORTATION = 'Transportation'
+    EDUCATION = 'Education'
+    ENTERTAINMENT = 'Entertainment'
+    UNEXPECTED = 'Unexpected'
 
 # Create your models here.
 class Income(models.Model):
@@ -21,9 +21,9 @@ class Income(models.Model):
 
 class Expense(models.Model):
     description = models.CharField(max_length=255)
-    value = models.DecimalField(max_digits=19, decimal_places=2)
+    value = models.DecimalField(max_digits=32, decimal_places=2)
     date = models.DateField()
-    category = models.IntegerField(max_length=1, choices=Category.choices, default=0)
+    category = models.CharField(max_length=128, choices=Category.choices, default=Category.OTHER)
 
     def __str__(self) -> str:
         return self.description
